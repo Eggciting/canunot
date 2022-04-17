42e41a703e3413e066e1642bbf29062732db340b
Whickoi <which.koi@gmail.com>
2022-04-16 20:05:03 -0400

    .

---
+++ gen.py
@@ -0,0 +1,23 @@
+import math
+import random
+
+def gen() -> int:
+    num = random.randrange(1, 3000)
+    dng = False
+    print(math.floor(num/2))
+    if num:
+        if input() == "no":
+            failed = [num]
+            for inserted in failed:
+                if num == inserted:
+                    dng = True
+            if dng:
+                # try generating another
+                sec_gen = random.randrange(num/2, 3000)
+                print(sec_gen)
+                return failed
+        elif input() == "yes" and not dng:
+            success = [num]
+            return success
+
+gen()
