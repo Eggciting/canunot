9bfd46484911b043ee78c14c581528a4293438df
Whickoi <which.koi@gmail.com>
2022-04-16 23:40:41 -0400

    fixed non-integer error tossing upon num generation

--- gen.py
+++ gen.py
@@ -11,11 +11,15 @@ def gen() -> int:
             for inserted in failed:
                 if num == inserted:
                     dng = True
-            if dng:
-                # try generating another
+        if dng:
+             # try generating another
+            try:
                 sec_gen = random.randrange(num/2, 3000)
                 print(sec_gen)
                 return failed
+            except:
+                print("Generatinga non-integer number... Canceling")
+
         elif input() == "yes" and not dng:
             success = [num]
             return success
