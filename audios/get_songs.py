18eaeb88da848baf9c113fed32846bedc7841cbe
Whickoi <which.koi@gmail.com>
2022-05-09 23:23:54 -0400

    work on printing actual songs

--- audios/get_songs.py
+++ audios/get_songs.py
@@ -1,10 +1,16 @@
 import os
-
-def read_audio_dir(self) -> str:
-    read = os.scandir(self)
-    for i in read:
-        print(i)
-        dir_entry_str: str = str(i)
-        return dir_entry_str
-
-read_audio_dir(os.curdir)
+class read:
+    def read_audio_dir(self) -> str:
+        read = os.scandir(self)
+        for i in read:
+            print(i)
+            dir_entry_str: str = str(i)
+            for ex in self:
+                if dir_entry_str != ex and len(self) < 1:
+                    clean: list = dir_entry_str.split("<DirEntry ")
+                    for i in clean:
+                        sec_clean: list = i.split(">")
+                        for u in sec_clean:
+                            return u
+                else:
+                    print("could not find song")
